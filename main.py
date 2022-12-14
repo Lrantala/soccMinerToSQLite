386d4abdb70182d18260ce035739fad7c3d22163
Leevi Rantala <leevirantala@gmail.com>
2022-12-14 12:56:31 +0200

    Added pmd as parameter, added changing of filename to match soccminer's style

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import logging
 import argparse
 import json
+import re
 from sqlite3_implementer.sqlite3_writer import SqliteWriter
 from directory_walker.directory_walker import DirectoryWalker
 
+
 def argument_parser():
     parser = argparse.ArgumentParser(description="Parser to read a filename from the command.")
-    parser.add_argument("-d", "--dir",
-                        help="Name of the path containing the json files, without / in the end", required=True)
+    parser.add_argument("-sd", "--soccdir",
+                        help="Name of the path containing the soccminer save location, without / in the end", required=False)
+    parser.add_argument("-pd", "--pmddir",
+                        help="Name of the path and file containing the json-file from pmd",
+                        required=False)
     parser.add_argument("-m", "--multiple", action="store_true", required=False,
                         help="Add this, if you want to analyze multiple repositories, which are in the given path.")
     parser.add_argument("-v", "--verbose", action="store_true", help="Whether to display logging information.")
     parser.add_argument("-n", "--name",
                         help="Name of the database file to be saved. Default: soccminer_db")
+    parser.add_argument("-a", "--analyzer",
+                        default="soccminer",
+                        const="soccminer",
+                        nargs="?",
+                        choices=["soccminer", "pmd"],
+                        help="Name of the analyzer used. Can be either 'soccminer' or 'pmd. Default: %(default)s",
+                        required=True)
     return parser
 
 
@@ -22,24 +34,36 @@ if __name__ == '__main__':
     if args.verbose:
         logging.basicConfig(level=logging.INFO)
 
-    directory_walker = DirectoryWalker()
-    directory_walker.list_directories_and_files(path_to_directory=args.dir)
+    analyzer = args.analyzer
 
-    sq3writer = SqliteWriter()
-    sq3writer.initialize_db()
+    if analyzer == "soccminer":
+        directory_walker = DirectoryWalker()
+        directory_walker.list_directories_and_files(path_to_directory=args.soccdir)
 
-    for file_location in directory_walker.list_of_dir_files:
-        with open(file_location, 'r') as f:
+        sq3writer = SqliteWriter()
+        sq3writer.initialize_db()
+
+        for file_location in directory_walker.list_of_dir_files:
+            with open(file_location, 'r') as f:
+                single_json = json.load(f)
+                directory_walker.list_of_json_files.extend(single_json)
+
+        # Cleaning up the unnecessasry json-data files, keeping only the ones with the comments.
+        directory_walker.list_of_json_files = [x for x in directory_walker.list_of_json_files if isinstance(x, dict)]
+        directory_walker.list_of_json_files = [x for x in directory_walker.list_of_json_files if len(x) == 17]
+        directory_walker.dict_to_tuple(dictionary=directory_walker.list_of_json_files)
+
+        # Writing the results into a db
+        sq3writer.insert_many_json_from_file_to_db(values=directory_walker.json_tuples)
+    elif analyzer == "pmd":
+        with open(args.pmddir, 'r') as f:
             single_json = json.load(f)
-            directory_walker.list_of_json_files.extend(single_json)
-
-    # Cleaning up the unnecessasry json-data files, keeping only the ones with the comments.
-    directory_walker.list_of_json_files = [x for x in directory_walker.list_of_json_files if isinstance(x, dict)]
-    directory_walker.list_of_json_files = [x for x in directory_walker.list_of_json_files if len(x) == 17]
-    directory_walker.dict_to_tuple(dictionary=directory_walker.list_of_json_files)
-
-    # Writing the results into a db
-    sq3writer.insert_many_json_from_file_to_db(values=directory_walker.json_tuples)
-
-
+        pmd_list_of_files = single_json["files"]
+        # Change the filenames to match the style in soccminer
+        for individual_file in pmd_list_of_files:
+            print(individual_file["filename"])
+            individual_file["filename"] = re.sub(".*argouml-VERSION_0_34\\\\", "", individual_file["filename"])
+            individual_file["filename"] = re.sub("\\\\", ".", individual_file["filename"])
+    else:
+        logging.info("Analyzer needs to be either 'soccminer' or 'pmd'.")
 
